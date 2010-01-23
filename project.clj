9fd5928e425674c3a02aea94b17df6bb8a1e81ba
Moritz Ulrich <ulrich.moritz@googlemail.com>
2010-01-24 00:19:52 +0100

    Added project.clj and resources

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject netwars "0.0.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.1.0"]])
