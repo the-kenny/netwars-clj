9dbdac5c7c205f52f25cae569c7f38c1f7337f60
Moritz Ulrich <ulrich.moritz@googlemail.com>
2010-01-24 15:46:10 +0100

    Added a loader for damagetable.xml. Same principe as in units.xml, but simpler

---
+++ src/netwars/damagetable.clj
@@ -0,0 +1,25 @@
+(ns netwars.damagetable
+  (:require [clojure.xml :as xml]
+            [clojure.zip :as zip])
+  (:use [clojure.set :only [rename-keys]]))
+
+(defn #^{:private true} mapvals [f m]
+  "Maps f over the values in a map"
+  (zipmap (keys m) (map f (vals m))))
+
+(defmulti #^{:private true} parse-element :tag)
+
+(defmethod #^{:private true} parse-element :unit
+  [{:keys [attrs content]}]
+  {(keyword (:name attrs)) (apply merge (map parse-element content))})
+
+(defmethod #^{:private true} parse-element :enemy
+  [{attrs :attrs}]
+  {(keyword (:name attrs))
+   (mapvals #(if % (read-string %))
+            (rename-keys
+             (select-keys attrs [:damage :alt_damage])
+             {:alt_damage :alt-damage}))})
+
+(defn parse-damagetable [xml-tree]
+  (map parse-element (:content xml-tree)))
