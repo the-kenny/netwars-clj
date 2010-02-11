0421dbdd61224fc8cbb95044af62515d39c20298
Moritz Ulrich <ulrich.moritz@googlemail.com>
2010-02-11 20:05:54 +0100

    Remove the graphing-lib

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject netwars "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
-                 [vijual "0.1.0-SNAPSHOT"]]
+                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.1.0"]])
