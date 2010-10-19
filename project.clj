99c429688c906a98b04e5784e3098639638e3457
Moritz Ulrich <ulrich.moritz@googlemail.com>
2010-10-19 21:38:45 +0200

    clojure 1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject netwars "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.0"]])
