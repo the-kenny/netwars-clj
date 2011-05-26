fcb151e243bb60ae38e92b8a23107f97af1470e7
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-05-26 10:30:50 +0200

    Slighly improved damagetable

--- src/netwars/damagetable.clj
+++ src/netwars/damagetable.clj
@@ -1,6 +1,5 @@
 (ns netwars.damagetable
-  (:require [clojure.xml :as xml]
-            [clojure.zip :as zip])
+  (:require [clojure.xml :as xml])
   (:use [clojure.set :only [rename-keys]]))
 
 (defn- mapvals
@@ -23,17 +22,13 @@
                       (select-keys attrs [:damage :alt_damage])
                       {:alt_damage :alt-damage}))))})
 
-(defn parse-damagetable [xml-tree]
+(defn- parse-damagetable [xml-tree]
   (apply merge (map parse-element (:content xml-tree))))
 
-(defn load-damagetable [file]
-  (parse-damagetable (xml/parse file)))
 
-(def *damagetable* (atom nil))
+(defn load-damagetable [source]
+  (parse-damagetable (xml/parse source) foo))
 
-(defn load-damagetable! [file]
-  (reset! *damagetable* (load-damagetable file)))
 
-(defn get-base-damage [att def]
-  (get-in @*damagetable* [att def]))
-
+(defn get-damage [table attacker defender]
+  (get-in table [attacker defender]))
