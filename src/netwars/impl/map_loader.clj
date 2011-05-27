a54179c689f6b24551a99a626662a29fd0a0fa80
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-05-27 23:14:36 +0200

    Move some stuff from netwars.map-loader to netwars.impl.map-loader

---
+++ src/netwars/impl/map_loader.clj
@@ -0,0 +1,35 @@
+(ns netwars.impl.map-loader
+  (:require [clojure.java.io :as io])
+  (:import java.nio.ByteBuffer))
+
+(defmacro read-when-possible [buf & body]
+  `(when (.hasRemaining ~buf)
+     ~@body))
+
+(defn read-binary-resource [#^String uri]
+  (with-open [bos (java.io.ByteArrayOutputStream.)
+              is (io/input-stream uri)]
+    (io/copy is bos)
+    (java.nio.ByteBuffer/wrap (.toByteArray bos))))
+
+(defn read-n-string [buf len]
+  (if (> len 0)
+   (let [arr (make-array Byte/TYPE len)]
+     (.get buf arr)
+     (apply str (map char  arr)))
+   ""))
+
+(defn read-null-string [buf]
+  (loop [s ""]
+	(if-let [c #(let [c (.getChar buf)] (if (= % 0) nil %))]
+	  (recur (str s c))
+	  s)))
+
+(defn read-byte [#^ByteBuffer buf]
+  (.get buf))
+
+(defn read-dword [#^ByteBuffer buf]
+  (.getShort buf))
+
+(defn read-int32 [#^ByteBuffer buf]
+  (.getInt buf))
--- src/netwars/map_loader.clj
+++ src/netwars/map_loader.clj
@@ -1,41 +1,8 @@
 (ns netwars.map-loader
-  (:require [clojure.contrib.str-utils2 :as str2]
-            [clojure.java.io :as io])
-  (:use [netwars.aw-map :only [coord]])
-  (:import java.nio.ByteBuffer
-           [netwars.aw-map AwMap AwMapUnit]))
-
-(defmacro read-when-possible [buf & body]
-  `(when (.hasRemaining ~buf)
-     ~@body))
-
-(defn read-binary-resource [#^String uri]
-  (with-open [bos (java.io.ByteArrayOutputStream.)
-              is (io/input-stream uri)]
-    (io/copy is bos)
-    (java.nio.ByteBuffer/wrap (.toByteArray bos))))
-
-(defn read-n-string [buf len]
-  (if (> len 0)
-   (let [arr (make-array Byte/TYPE len)]
-     (.get buf arr)
-     (apply str (map char  arr)))
-   ""))
-
-(defn read-null-string [buf]
-  (loop [s ""]
-	(if-let [c #(let [c (.getChar buf)] (if (= % 0) nil %))]
-	  (recur (str s c))
-	  s)))
-
-(defn read-byte [#^ByteBuffer buf]
-  (.get buf))
-
-(defn read-dword [#^ByteBuffer buf]
-  (.getShort buf))
-
-(defn read-int32 [#^ByteBuffer buf]
-  (.getInt buf))
+  (:require [clojure.contrib.str-utils2 :as str2])
+  (:use [netwars.aw-map :only [coord]]
+        netwars.impl.map-loader)
+  (:import [netwars.aw-map AwMap AwMapUnit]))
 
 (let [tilesets [:normal :snow :desert :wasteland :aw1 :aw2]]
   (defn parse-tileset [byte]
