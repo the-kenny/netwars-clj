6870ebdd47983596b1752b92b5e2d5f7988166c0
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-05-29 19:57:12 +0200

    Test maps in other formats

--- test/netwars/map_loader_tests.clj
+++ test/netwars/map_loader_tests.clj
@@ -1,21 +1,22 @@
 (ns netwars.map-loader-tests
-  (:use [lazytest.describe :only [describe given testing it with]]
+  (:use [lazytest.describe :only [describe do-it given testing it with]]
+        [lazytest.expect :only [expect]]
         netwars.map-loader
         netwars.aw-map)
   (:import [netwars.aw-map AwMap]))
 
 (describe load-map
-  (given [map-file "/Users/moritz/Development/Clojure/netwars/7330.aws"]
+  (given [map-file "/Users/moritz/Development/Clojure/netwars/maps/7330.aws"]
     (it "loads a map from map-file"
       (instance? AwMap (load-map map-file)))))
 
-(describe "A loaded map from 7330.aws" 
-  (given [map-file "/Users/moritz/Development/Clojure/netwars/7330.aws"
+(describe ".aws map loading" 
+  (given [map-file "/Users/moritz/Development/Clojure/netwars/maps/7330.aws"
           loaded-map (load-map map-file)]
-    (it "has a width > 0"
-      (pos? (width loaded-map)))
-    (it "has a height > 0"
-      (pos? (height loaded-map)))
+    (it "has a width of 20"
+      (= 20 (width loaded-map)))
+    (it "has a height of 15"
+      (= 15 (height loaded-map)))
     (it "has width columns in :terrain"
       (= (count (:terrain loaded-map)) (width loaded-map)))
     (it "has height items in every row"
@@ -42,3 +43,58 @@
         (let [unit (unit-at loaded-map (coord 18 14))]
           (and (= 21 (:id unit))
                (= :black (:color unit))))))))
+
+(describe ".aw2 map loading" 
+  (given [map-file "/Users/moritz/Development/Clojure/netwars/maps/0035.aw2"
+          loaded-map (load-map map-file)]
+    (it "has a width of 30"
+      (= 30 (width loaded-map)))
+    (it "has a height of 20"
+      (= 20 (height loaded-map)))
+    (it "has width columns in :terrain"
+      (= (count (:terrain loaded-map)) (width loaded-map)))
+    (it "has height items in every row"
+      (every? #(= (height loaded-map) (count %)) (:terrain loaded-map)))
+
+    (testing terrain-at
+      (it "returns :water at [0 0]"
+        (= :water (terrain-at loaded-map (coord 0 0))))
+      (it "returns :mountain at [28 19]"
+        (= :mountain (terrain-at loaded-map (coord 28 19))))
+      (it "returns [:headquarter :red] at [1 14]"
+        (= [:headquarter :red] (terrain-at loaded-map (coord 1 14)))))
+    
+    (testing unit-at
+      (it "returns an AwMapUnit with id 7 (Battleship) and color :red at [1 4]"
+        (let [unit (unit-at loaded-map (coord 1 4))]
+          (and (= 7 (:id unit))
+               (= :red (:color unit)))))
+      (it "returns an AwMapUnit with id 23 (Rockets) and color :yellow at [18 10]"
+        (let [unit (unit-at loaded-map (coord 18 10))]
+          (and (= 23 (:id unit))
+               (= :yellow (:color unit))))))))
+
+(describe ".awm map loading" 
+  (given [map-file "/Users/moritz/Development/Clojure/netwars/maps/1795.awm"
+          loaded-map (load-map map-file)]
+    (it "has a width of 30"
+      (= 30 (width loaded-map)))
+    (it "has a height of 20"
+      (= 20 (height loaded-map)))
+    (it "has width columns in :terrain"
+      (= (count (:terrain loaded-map)) (width loaded-map)))
+    (it "has height items in every row"
+      (every? #(= (height loaded-map) (count %)) (:terrain loaded-map)))
+
+    (testing terrain-at
+      (it "returns :water at [0 0]"
+        (= :water (terrain-at loaded-map (coord 0 0))))
+      (it "returns :mountain at [5 11]"
+        (= :mountain (terrain-at loaded-map (coord 5 11))))
+      (it "returns [:headquarter :blue] at [7 13]"
+        (= [:headquarter :blue] (terrain-at loaded-map (coord 7 13)))))
+    
+    (testing unit-at
+      (do-it "returns nil for all coordinates"
+        (doseq [x (range (width loaded-map)) y (range (height loaded-map))]
+          (expect (nil? (unit-at loaded-map (coord x y)))))))))
