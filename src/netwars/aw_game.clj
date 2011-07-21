1bec81228f9e09d515f1cda840735c815b58ced3
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-07-21 22:05:35 +0200

    Add game-events

--- src/netwars/aw_game.clj
+++ src/netwars/aw_game.clj
@@ -15,18 +15,37 @@
   (let [loaded-map (netwars.map-loader/load-map mapsource)
         unit-spec (netwars.unit-loader/load-units "resources/units.xml")
         board (generate-game-board loaded-map unit-spec)
-        newplayers (clojure.core/map ref players)]
-    (AwGame. (assoc info :map mapsource)
+        newplayers (clojure.core/map ref players)
+        newinfo (assoc info :map mapsource)
+        initial-event {:type :game-started
+                       :info newinfo
+                       :loaded-map loaded-map
+                       :unit-spec unit-spec
+                       :players players}]
+    (AwGame. newinfo
              (ref 0)
              newplayers
              unit-spec
              (ref board)
-             (ref []))))
+             (ref [initial-event]))))
+
+;;; Game events
+
+(defn game-events [game]
+  @(:moves game))
+
+(defn log-event! [game move]
+  {:pre [(contains? move :type)]}
+  (alter (:moves game) conj move))
+
+;;; Player Functions
 
 (defn current-player [game]
   (nth (:players game) @(:current-player-index game)))
 
 (defn next-player! [game]
+  (log-event! game {:type :turn-completed
+                    :player @(current-player game)})
   (alter (:current-player-index game)
          (fn [idx]
            (if (>= (inc idx) (count (:players game)))
--- test/netwars/test/aw_game.clj
+++ test/netwars/test/aw_game.clj
@@ -11,7 +11,19 @@
                         (f))))
 
 (deftest test-aw-game
-  (is (instance? netwars.aw_game.AwGame *game*)))
+  (is (instance? netwars.aw_game.AwGame *game*))
+  (is (= :game-started (-> *game* :moves deref first :type))))
+
+(deftest test-action-logging
+  (is (thrown? java.lang.IllegalStateException
+               (log-event! *game* {:type 42})))
+  (dosync
+   (is (thrown? java.lang.AssertionError
+                (log-event! *game* {})))
+
+   (is (= 1 (count (game-events *game*))))
+   (log-event! *game* {:type :foobar})
+   (is (= {:type :foobar} (second (game-events *game*))))))
 
 (deftest test-player-functions
   (dosync
@@ -24,4 +36,8 @@
    (is (= :player1 @(current-player *game*))))
 
   (is (thrown? java.lang.IllegalStateException (next-player! *game*))
-      "Throws exception outside of transaction"))
+      "Throws exception outside of transaction")
+
+  (is (= 4 (count (game-events *game*))))
+  (doseq [event (rest (game-events *game*))]
+    (is (= :turn-completed (:type event)))))
