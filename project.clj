2cd0212f45c0f61a577fda2bea521be30b7ecc7b
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-07-22 01:01:14 +0200

    aleph and common-codec

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject netwars "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [incanter/incanter-processing "1.2.3"]]
+                 [aleph "0.2.0-alpha2"]
+                 [commons-codec "1.5"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"})
