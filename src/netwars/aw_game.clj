62ed1b9d7c931eba75c25e72a7a9a361058dff1a
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-07-23 14:05:48 +0200

    New function: perform-attack!
    Performs an attack and the corresponding counterattack

--- src/netwars/aw_game.clj
+++ src/netwars/aw_game.clj
@@ -1,6 +1,8 @@
 (ns netwars.aw-game
   (:use [netwars.game-board :as board]
-        [netwars.aw-unit :as unit])
+        [netwars.aw-unit :as unit]
+        [netwars.damagecalculator :as damage]
+        [netwars.damagetable :as damagetable])
   (:require netwars.map-loader
             netwars.unit-loader))
 
@@ -8,6 +10,7 @@
                    current-player-index
                    players
                    unit-spec
+                   damagetable
                    board
                    moves                ;Every move in the game gets saved here
                    ])
@@ -15,6 +18,7 @@
 (defn make-game [info mapsource players]
   (let [loaded-map (netwars.map-loader/load-map mapsource)
         unit-spec (netwars.unit-loader/load-units "resources/units.xml")
+        damagetable (damagetable/load-damagetable "resources/damagetable.xml")
         board (board/generate-game-board loaded-map unit-spec)
         newplayers (clojure.core/map ref players)
         newinfo (assoc info :map mapsource)
@@ -27,6 +31,7 @@
              (ref 0)
              newplayers
              unit-spec
+             damagetable
              (ref board)
              (ref [initial-event]))))
 
@@ -56,3 +61,23 @@
 
 ;;; Attacking
 
+(defn perform-attack! [game att-coord vic-coord & {:keys [counterattack]}]
+  {:pre [(board/in-attack-range? @(:board game) att-coord vic-coord)]}
+  (let [board @(:board game)
+        att (board/get-unit board att-coord)
+        vic (board/get-unit board vic-coord)
+        att-terr (board/get-terrain board att-coord)
+        vic-terr (board/get-terrain board vic-coord)]
+    (let [dam (damage/calculate-damage (:damagetable game)
+                                       [att att-terr]
+                                       [vic vic-terr])
+          newvic (unit/apply-damage vic dam)]
+      (if newvic
+       (alter (:board game) board/update-unit vic-coord #(unit/apply-damage % dam))
+       (alter (:board game) board/remove-unit vic-coord))
+      (when (and newvic
+                 (board/in-attack-range? @(:board game) vic-coord att-coord)
+                 (not counterattack))
+        (perform-attack! game vic-coord att-coord :counterattack true))
+      ;; TODO: Logging of events
+      game)))
--- test/netwars/test/aw_game.clj
+++ test/netwars/test/aw_game.clj
@@ -1,6 +1,7 @@
 (ns netwars.test.aw-game
   (:use clojure.test
         netwars.aw-game
+        [netwars.game-board :as board]
         [netwars.aw-map :only [coord]]))
 
 (def *game* nil)
@@ -43,3 +44,26 @@
   (is (= 4 (count (game-events *game*))))
   (doseq [event (rest (game-events *game*))]
     (is (= :turn-completed (:type event)))))
+
+(deftest test-perform-attack!
+  (let [artillery (coord 1 11)
+        infantry (coord 1 13)]
+    (dosync
+     (is (= 10 (:hp (board/get-unit @(:board *game*) artillery))))
+     (is (= 10 (:hp (board/get-unit @(:board *game*) infantry))))
+     (perform-attack! *game* artillery infantry)
+     (is (= 10 (:hp (board/get-unit @(:board *game*) artillery))))
+     (is (= 5 (:hp (board/get-unit @(:board *game*) infantry)))))))
+
+(deftest test-counter-attack
+  (let [infantry (coord 1 13)
+        infantry2 (coord 2 13)]
+    (dosync
+     ;; Add a new infantry next to another for testing
+       (alter (:board *game*) board/add-unit infantry2
+            (board/get-unit @(:board *game*) infantry))
+     (is (= 10 (:hp (board/get-unit @(:board *game*) infantry))))
+     (is (= 10 (:hp (board/get-unit @(:board *game*) infantry2))))
+     (perform-attack! *game* infantry infantry2)
+     (is (contains? #{9 8} (:hp (board/get-unit @(:board *game*) infantry))))
+     (is (contains? #{4 5} (:hp (board/get-unit @(:board *game*) infantry2)))))))
