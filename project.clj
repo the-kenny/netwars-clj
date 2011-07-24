371ad347b35f4960d0cf2a1473cb1d4d9f51993a
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-07-24 14:14:20 +0200

    move clojure source to src/clj/

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [aleph "0.2.0-alpha2"]
                  [commons-codec "1.5"]]
-  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"})
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
+  :source-path "src/clj/")
