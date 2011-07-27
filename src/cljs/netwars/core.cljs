fbfca8af46315226adc1c92b3eaf92f1bb4372f6
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-07-27 02:44:02 +0200

    use new over the wire protocol (pr-str and read-str; have to make this safe)

--- src/cljs/netwars/core.cljs
+++ src/cljs/netwars/core.cljs
@@ -1,8 +1,8 @@
 (ns netwars.core
   (:require [goog.events :as events]
             [goog.dom :as dom]
-            [goog.json :as json]
-            [netwars.drawing :as drawing]))
+            [netwars.drawing :as drawing]
+            [netwars.connection :as connection]))
 
 ;;; Logging Stuff
 
@@ -17,24 +17,16 @@
 (defn set-connection-status [status]
   (dom/setTextContent (dom/getElement "connectionIndicator") status))
 
-(defn on-message [event]
-  (let [obj (json/parse (.data event))
-        encoded-map (.map-image obj)
-        units (.units obj)]
-    (when-let [message (.message obj)]
-     (log-message (str "got message: " message)))
-    (log-message (str (.data event)))
-    (when units
-      (log-message (str "got " units " units")))
-    (when encoded-map
-      (drawing/draw-terrain board-context encoded-map))))
+(def socket (connection/open-socket "ws://localhost:8080"))
 
-(defn start-socket [uri]
-  (let [ws (js/WebSocket. uri)]
-    ;; (set! (. ws onopen) #(set-connection-status "opened!"))
-    (events/listen ws "open" #(set-connection-status "connected"))
-    (events/listen ws "close" #(set-connection-status "closed"))
-    ;; (events/listen ws "message" on-message)
-    (set! (. ws onmessage) on-message)))
+(defn request-map-data [m]
+  (connection/send-data socket {"type" "request-map", "map" m}
+                        (fn [obj]
+                          (drawing/draw-terrain board-context
+                                                (get obj "map-data")))))
 
-(start-socket "ws://localhost:8080")
+(events/listen (dom/getElement "request-button") events/EventType.CLICK
+               #(do (connection/log "Clicked request-button")
+                    (request-map-data "7330.aws")))
+
+;(request-map-data "7330.aws")
--- src/wstest.clj
+++ src/wstest.clj
@@ -3,15 +3,31 @@
 
 (def broadcast-channel (permanent-channel))
 
+(defn send-data [ch data]
+  (enqueue ch (pr-str (into {} (for [[k v] data] [(name k) v])))))
+
+(def foo (atom []))
 (defn register-game-handlers [client-channel]
- ;; (receive-all client-channel #(if (= )))
-  (siphon broadcast-channel client-channel))
+  ;; (receive-all client-channel #(if (= )))
+  (siphon broadcast-channel client-channel)
+  (receive-all client-channel
+               #(let [v (read-string %)]
+                  (println v)
+                  (when (= (get v "type") "request-map")
+                    (send-data
+                     client-channel
+                     {"id" (get v "id")
+                      "map-data"
+                      (serve-terrain-image (map-loader/load-map "maps/7330.aws"))}))))
+  )
 
 (defn client-handler [ch handshake]
-  (enqueue ch {:welcome  "Welcome to Netwars!"})
-  (register-game-handlers ch))
+  (send-data ch {:id "welcome-message"
+                 :welcome "Welcome to Netwars!"})
+  (#'register-game-handlers ch))
 
-(def server (start-http-server #'client-handler {:port 8080 :websocket true}))
+(defonce server
+  (start-http-server #'client-handler {:port 8080 :websocket true}))
 
 ;;; Netwars stuff
 (use '[clojure.contrib.base64 :as base64]
@@ -35,12 +51,15 @@
 
 (defn unit-to-json [units]
   (formats/data->json->string
-   (reduce #(assoc-in %1 [(:x (key %2)) (:y (key %2))] (val %2)) {} units)))
+   (seq units)
+   ;; (reduce #(assoc-in %1 [(:x (key %2)) (:y (key %2))] (val %2)) {} units)
+   ))
 
 (let [loaded-map (map-loader/load-map "maps/7330.aws")
       unit-spec  (unit-loader/load-units "resources/units.xml")
       board      (board/generate-game-board loaded-map unit-spec)]
-  (println (unit-to-json (:units board)))
- (enqueue broadcast-channel
-          (json/json-str {:map_image (serve-terrain-image loaded-map)
-                          :units (unit-to-json (:units board))})))
+  (enqueue broadcast-channel
+           (json/json-str {:type :initialize-map
+                           :map_image (serve-terrain-image loaded-map)
+                           :units (unit-to-json (:units board))
+                           :message "Starting game..."})))
