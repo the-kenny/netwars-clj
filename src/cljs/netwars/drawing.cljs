8e4b8302e4eb45cf4308b32d3ebdf714af5fd267
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-07-27 02:45:22 +0200

    add some graphics-related caching fns

--- src/cljs/netwars/drawing.cljs
+++ src/cljs/netwars/drawing.cljs
@@ -1,5 +1,24 @@
 (ns netwars.drawing)
 
+;;; Image loading and caching
+
+(let [cache (atom {})]
+ (defn cache-image! [key image-data]
+   (let [image (js/Image.)]
+     (set! (. image src) image-data)
+    (swap! cache assoc key image)))
+
+ (defn get-cached-image [key]
+   (when-let [img (get cache key)]
+     img)))
+
+;; (defn request-image [server key & [callback]]
+;;   (.send socket (js/JSON/stringify {:type :request-image
+;;                                     :image key
+;;                                     :request-id })))
+
+;;; Canvas functions
+
 (defn make-graphics [element]
   {:context (.getContext element "2d")
    :canvas  element})
@@ -12,16 +31,20 @@
 (defn clear [graphics]
   (.clear (:context graphics)))
 
+;;; Netwars specific drawing functions
+
 (defn draw-terrain [graphics image-data]
   (let [image (js/Image.)
         canvas (:canvas graphics)]
     (set! (. image src) image-data)
-    (resize-graphics graphics (.width image) (.height image))
+    (when (and (pos? (.width image)) (pos? (.height image)))
+     (resize-graphics graphics (.width image) (.height image)))
     (.drawImage (:context graphics) image
                 (/ (- (.width canvas) (.width image)) 2)
                 (/ (- (.height canvas) (.height image)) 2))))
 
-(defn draw-image-at [graphics x y data]
+
+(defn draw-unit-at [graphics x y data]
   (let [image (js/Image.)
         canvas (:canvas graphics)]
     (set! (. image src) data)
