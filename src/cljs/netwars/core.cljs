5126f7a8b18786d2ea6392b839c1af6ecd975b0e
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-08-03 18:48:45 +0200

    call drawing fn every 1/10 second

--- src/cljs/netwars/core.cljs
+++ src/cljs/netwars/core.cljs
@@ -51,6 +51,6 @@
 ;;(drawing/start-animation board-context)
 
 ;;; TODO: Use animation when enormous cpu usage is fixed
-(let [t (goog.Timer. 1000)]
+(let [t (goog.Timer. 100)]
   (events/listen t goog.Timer/TICK #(drawing/redraw board-context))
   (. t (start)))
