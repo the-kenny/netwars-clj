811a9c8e6d948ae21234a364fbca9178f5e1f5c8
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-08-14 16:10:09 +0200

    compojure in project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
                  [commons-codec "1.5" :exclusions [commons-logging]]
                  [org.clojure/tools.logging "0.2.0"]
                  [log4j/log4j "1.2.16"]
-                 [clj-logging-config "1.5"]]
+                 [clj-logging-config "1.5"]
+                 [compojure "0.6.5"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
   :source-path "src/clj/"
   :main netwars.core)
