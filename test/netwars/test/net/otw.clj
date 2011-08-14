d90af92e76010bb561ba97bc17d29f53bb5c6540
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-08-14 14:17:08 +0200

    deleted otw tests

--- test/netwars/test/net/otw.clj
+++
@@ -1,8 +0,0 @@
-(ns netwars.test.net.otw
-  (:use clojure.test
-        netwars.net.otw))
-
-(deftest test-encoding
-  (is (= (encode-data [1 2 3]) "(1 2 3)"))
-  (is (= (encode-data {:foo 42}) "{:foo 42}"))
-  (is (= "^{:foo 42} {}" (encode-data (with-meta {} {:foo 42})))))
