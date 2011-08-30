608cabad8731fad11d88ff992930607aef274128
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-08-30 23:10:46 +0200

    Don't use atoms where they aren't necessary

--- src/cljs/netwars/game.cljs
+++ src/cljs/netwars/game.cljs
@@ -7,9 +7,9 @@
 ;; {:game-id nil
 ;;  :info {:map-name nil
 ;;         :players []}}
-(def running-game (atom nil))
-(def game-units (atom nil))
-(def terrain-image (atom nil))
+(def running-game nil)
+(def game-units nil)
+(def terrain-image nil)
 
 
 ;;; Game state
@@ -19,13 +19,14 @@
 ;;; General methods for games
 
 (defn unit-at [c]
-  (get @game-units c))
+  (get game-units c))
 
 ;;; TODO: pre/post
 (defn move-unit [from to]
   (let [u (unit-at from)]
-    (swap! game-units dissoc from)
-    (swap! game-units assoc to u)))
+    (set! game-units (-> game-units
+                         (dissoc from)
+                         (assoc to u)))))
 
 (defn clicked-on [[x y]]
   (logging/log "clicked on: " x "/" y)
@@ -60,8 +61,8 @@
 ;;; Handling of responses for new games
 
 (defmethod connection/handle-response :game-data [message]
-  (reset! running-game {:game-id (:game-id message)
-                        :info (:info message)}))
+  (set! running-game {:game-id (:game-id message)
+                      :info (:info message)}))
 
 (defn join-game [game-id]
   (connection/send-data {:type :join-game
@@ -85,21 +86,21 @@
 
 (defmethod connection/handle-response :request-map [message]
    ;; (drawing/draw-terrain board-context (get message :map-data))
-  (drawing/image-from-base64 (:map-data message) #(reset! terrain-image %)))
+  (drawing/image-from-base64 (:map-data message) #(set! terrain-image %)))
 
 (defmethod connection/handle-response :unit-data [data]
   (logging/log "got " (count (:units data)) " units")
-  (reset! game-units (:units data)))
+  (set! game-units (:units data)))
 
 ;;; Concrete drawing functions
 
 (defn draw-game [graphics]
-  (when (and @running-game @game-units @terrain-image)
+  (when (and running-game game-units terrain-image)
     (drawing/clear graphics)
     ;; Draw the map
-    (drawing/draw-terrain-image graphics @terrain-image)
+    (drawing/draw-terrain-image graphics terrain-image)
     ;; Draw the units
-    (doseq [[c u] @game-units]
+    (doseq [[c u] game-units]
       (drawing/draw-unit-at graphics u c))
     ;; Draw the movement-range
     (doseq [c movement-range]
