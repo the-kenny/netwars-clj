476964bdd23e2c7e5e04262d26d3b02496150ef0
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-12-25 22:28:37 +0100

    add method to add an element to the dom

--- src/cljs/netwars/logging.cljs
+++ src/cljs/netwars/logging.cljs
@@ -4,13 +4,16 @@
 (defn log [& more]
   (.log js/console (apply str more)))
 
-(defn message [& more]
+(defn message-html [elem]
   (let [messageLog (dom/getElement "messageLog")]
     (dom/appendChild messageLog
-     (dom/createDom "div" nil (apply str more)))
+     (dom/createDom "div" nil elem))
     ;; Scroll to bottom found at:
     ;; http://www.ajax-community.de/javascript/6065-div-anspringen-timeout-berbr-cken.html
     (set! (. messageLog scrollTop) (.scrollHeight messageLog))))
+
+(defn message [& more]
+  (message-html (apply str more)))
 
 (defn clear-messages []
   (dom/removeChildren (dom/getElement "messageLog")))
