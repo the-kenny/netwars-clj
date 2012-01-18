82d87d0fd7a4b3512b480ac96d67fc9a81430b8d
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-01-18 18:30:05 +0100

    tests for netwars.net.rest

---
+++ test/netwars/net/test/rest.clj
@@ -0,0 +1,24 @@
+(ns netwars.net.test.rest
+  (:use midje.sweet
+        netwars.net.rest
+        [netwars.core :as core]
+        [netwars.net.game-server :as game-server]
+        [clojure.data.json :as json]))
+
+(background (before :contents (do
+                                (core/start)
+                                (game-server/start-new-game {:map-name "7330.aws"})))
+            (after  :contents (core/stop)))
+
+(fact
+  (json/read-json (main)) => {:api-version 1.0})
+
+(fact
+  (json/read-json (games)) =>  (contains {:count integer? :ids sequential?}))
+
+(fact
+  (when-let [id (first (:ids (json/read-json (games))))]
+    (json/read-json (game id)) => (contains {:info map?
+                                             :players sequential?
+                                             :moves sequential?
+                                             :current-player-index integer?})))
