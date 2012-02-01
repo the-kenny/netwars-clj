17a2d20d6c44cf3e1e1c1b0e61249614b6415aa9
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-01 19:52:16 +0100

    fix project by using compojure 1.0.1

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject netwars "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [aleph "0.2.1-SNAPSHOT"]
+                 [aleph "0.2.1-alpha2-SNAPSHOT"]
                  [commons-codec "1.5" :exclusions [commons-logging]]
                  [org.clojure/tools.logging "0.2.0"]
                  [log4j/log4j "1.2.16"]
                  [clj-logging-config "1.9.6"
                   :exclusions [swank-clojure/swank-clojure]]
-                 [compojure "1.0.0" :exclusions [org.clojure/clojure]]
+                 [compojure "1.0.1" :exclusions [org.clojure/clojure]]
                  [ring/ring-devel "1.0.1"]
                  [org.clojure/data.json "0.1.1"]]
   :dev-dependencies [[lein-cljsbuild "0.0.8"]
