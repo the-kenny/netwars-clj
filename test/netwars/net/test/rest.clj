426923a158072bf8c458eaa9623e6adf8070cb8a
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-03 01:11:22 +0100

    extend (and fix) tests for api

--- test/netwars/net/test/rest.clj
+++ test/netwars/net/test/rest.clj
@@ -5,22 +5,31 @@
         [netwars.net.game-server :as game-server]
         [clojure.data.json :as json]))
 
-(background (before :contents (do
-                                (core/start)
-                                (game-server/start-new-game {:map-name "7330.aws"})))
-            (after  :contents (core/stop)))
+(defn- start-dummy-game []
+  (dosync
+   (game-server/store-game! (-> (game-server/start-new-game {:map-name "7330.aws"})
+                                (assoc :game-id (java.util.UUID/randomUUID))))))
 
-(fact
-  (json/read-json (main)) => {:api-version 1.0})
+;;; NOTE: There seems to be a bug. This doesn't work if you use `background' instead of
+;;;       `against-background'.
+(against-background [(before :contents (do
+                                         (core/start)
+                                         (start-dummy-game)))
+                     (after :contents (core/stop))]
 
-(fact
-  (json/read-json (games)) =>  (contains {:count integer? :ids sequential?}))
+  (fact
+    (json/read-json (main)) => {:api-version 1.0})
 
-(fact
-  (let [id (first (:ids (json/read-json (games))))]
-    id => string?
-    (json/read-json (game id)) => (contains {:info map?
-                                             :players sequential?
-                                             :moves sequential?
-                                             :current-player-index integer?
-                                             :round-counter integer?})))
+  (fact
+    (json/read-json (games)) => (contains {:count integer? :ids sequential?})
+    (count (:ids (json/read-json (games)))) => 1 ;make sure we have a game running
+    )
+
+  (fact
+    (let [id (first (:ids (json/read-json (games))))]
+      id => string?
+      (json/read-json (game id)) => (contains {:info map?
+                                               :players sequential?
+                                               :moves sequential?
+                                               :current-player-index integer?
+                                               :round-counter integer?}))))
