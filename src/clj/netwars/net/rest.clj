cfc2ea8b8d032c30b7d7573acfb551e4a51c0f73
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-04-19 02:36:14 +0200

    Serve map backgrounds via http

--- src/clj/netwars/net/rest.clj
+++ src/clj/netwars/net/rest.clj
@@ -2,12 +2,13 @@
   (:use compojure.core
         clojure.data.json
         ;; [netwars.net.game-server :as server]
-        )
+        [clojure.java.io :only [input-stream output-stream]])
   (:require [netwars.aw-game :as aw-gameq]
             [netwars.damagetable :as damagetable]
             [netwars.unit-loader :as unit-loader]
             [netwars.game-creator :as game-creator]
-            [netwars.net.otw :as otw])
+            [netwars.net.otw :as otw]
+            [netwars.map-drawer :as map-drawer])
   (:import [netwars.aw_map Coordinate]))
 
 ;;; Serialization
@@ -42,9 +43,32 @@
   (json-str (unit-loader/load-units "resources/units.xml")))
 ;; (alter-var-root #'unit-spec memoize)
 
+;;; Map stuff
+
+(defn- map-url [map-name]
+  (str "/api/render-map/" map-name))
+
+(defn render-map [map-name]
+  ;; TODO: This performs badly
+  (let [game (game-creator/make-game {} (str "maps/" map-name))
+        image (map-drawer/render-terrain-board (-> game :board :terrain))
+        in (java.io.PipedInputStream.)]
+    ;; Now this is fun stuff:
+    ;; We throw off a future which writes data to the output stream
+    ;; which then feeds this data to in which is read by Ring
+    (future (with-open [out (java.io.PipedOutputStream. in)]
+              (javax.imageio.ImageIO/write image "png" out)))
+    {:status 200
+     :headers {"Content-Type" "image/png"}
+     :body in}))
+
 (defn make-game [map-name]
-  (otw/encode-data (dissoc (game-creator/make-game {} (str "maps/" map-name))
-                           :moves))) ;; (defn games []
+  (->  (game-creator/make-game {} (str "maps/" map-name))
+       (dissoc :moves)
+       (assoc  :map-url (map-url map-name))
+       (otw/encode-data)))
+
+;; (defn games []
 ;;   (let [ids (keys @server/running-games)]
 ;;     (json-str {:count (count ids)
 ;;                :ids (vec ids)})))
@@ -64,6 +88,7 @@
   (GET "/damagetable.json" [] (damagetable))
   (GET "/unit-spec.json" [] (unit-spec))
   (GET "/new-game/:map-name" [map-name] (make-game map-name))
+  (GET "/render-map/:map-name" [map-name] (render-map map-name))
   ;; (GET "/games" [] (games))
   ;; (GET "/game/:id" [id] (game id))
   )
