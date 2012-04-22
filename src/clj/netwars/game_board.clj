409cd6287ae8d164f9ce697aa26219e01af11de1
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-04-22 21:20:43 +0200

    micro optimization for reachable-fields

--- src/clj/netwars/game_board.clj
+++ src/clj/netwars/game_board.clj
@@ -114,17 +114,18 @@
 (defn reachable-fields
   "Returns a set of all coordinates reachable by unit on coordinate c"
   [board c]
+  (assert (get-unit board c))
+  (assert (meta (get-unit board c)))
   (let [unit (get-unit board c)
         movement-range (:movement-range (meta unit))
         movement-type (:movement-type (meta unit))
         fuel (:fuel unit)
+        terrain (:terrain board)
         acc (atom #{})]
-    (assert (meta unit))
-    (let [helper (fn helper [c rest & {:keys [initial?]}]
+    (let [helper (fn helper [c rest initial?]
                    (let [t (get-terrain board c)
                          costs (if initial? 0 (aw-map/movement-costs t movement-type))]
                      (when (and
-                            (aw-map/in-bounds? (:terrain board) c)
                             (aw-map/can-pass? t movement-type)
                             (or initial? (can-walk-on-field? board unit c))
                             (>= rest costs))
@@ -136,9 +137,13 @@
                                 down  (aw-map/coord      x (inc y))
                                 up    (aw-map/coord      x (dec y))
                                 restcosts (- rest costs)]
-                            (helper right restcosts)
-                            (helper left  restcosts)
-                            (helper down  restcosts)
-                            (helper up    restcosts)))))))]
-      (helper c (min movement-range fuel) :initial? true))
+                            (when (aw-map/in-bounds? terrain right)
+                              (helper right restcosts false))
+                            (when (aw-map/in-bounds? terrain left)
+                              (helper left  restcosts false))
+                            (when (aw-map/in-bounds? terrain down)
+                              (helper down  restcosts false))
+                            (when (aw-map/in-bounds? terrain up)
+                             (helper up    restcosts false))))))))]
+      (helper c (min movement-range fuel) true))
     @acc))
