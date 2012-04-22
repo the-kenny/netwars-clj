9ee81dda7872488332dac5a3db3368223dc68a4d
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-04-22 21:20:25 +0200

    add one more test for reachable-fields

--- test/netwars/test/game_board.clj
+++ test/netwars/test/game_board.clj
@@ -54,16 +54,44 @@
   (let [reachable (reachable-fields (make-testboard) (coord 1 11))]
     (is (set? reachable))
     (is (not (empty? reachable)))
-    (is (= reachable (set (map #(apply coord %)
+    (is (= reachable (set (map coord
                                [[0 13] [1 14] [2 14] [0 12] [1 12] [2 13]
                                 [2 14] [0 11] [2 12] [1 11] [0 10] [3 13]
                                 [3 12] [4 13] [0 9]  [4 12] [3 11] [0 8] [1 9]])))))
+
   (let [reachable (reachable-fields (make-testboard) (coord 4 13))]
     (is (set? reachable))
     (is (not (empty? reachable)))
-    (is (= reachable
-           (set (map #(apply coord %)
-                     [[1 14] [0 12] [1 12] [2 14] [5 13]
-                      [2 13] [1 11] [2 12] [3 13] [4 14]
-                      [3 12] [4 13] [3 11] [4 12]
-                      ]))))))
+    (is (= reachable (set (map coord
+                               [[1 14] [0 12] [1 12] [2 14] [5 13]
+                                [2 13] [1 11] [2 12] [3 13] [4 14]
+                                [3 12] [4 13] [3 11] [4 12]])))))
+
+
+  (let [reachable (reachable-fields (make-testboard) (coord 13 2))]
+    (is (set? reachable))
+    (is (not (empty? reachable)))
+    (is (= reachable (set (map coord
+                               [[18 1] [17 0] [8 3] [9 4] [10 5]
+                                [11 6] [12 7] [9 3] [10 4] [11 5]
+                                [12 6] [13 7] [10 3] [12 5] [11 4]
+                                [13 6] [10 2] [11 3] [12 4] [13 5]
+                                [14 6] [9 1] [11 2] [12 3] [13 4]
+                                [14 5] [10 1] [15 6] [12 2] [13 3]
+                                [14 4] [11 1] [15 5] [10 0] [13 2]
+                                [14 3] [12 1] [15 4] [11 0] [14 2]
+                                [13 1] [15 3] [12 0] [14 1] [13 0]
+                                [15 1] [14 0] [15 0] [16 5] [16 4]
+                                [16 3] [17 4] [17 3] [16 2] [16 1]
+                                [18 3] [17 2] [17 1] [16 0]]))))))
+
+(defn test-movement-range-performance []
+  (let [board (make-testboard)]
+    (time
+     (dotimes [_ 100]
+       (reachable-fields (make-testboard) (coord 1 11))))))
+;; (test-movement-range-performance) => 1630.004 msecs
+
+(defn time-test-movement-range []
+  (test-movement-range)
+  (test-movement-range-performance))
