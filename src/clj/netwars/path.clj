d6f11652c5f0a8ef7f97139ed20cf85722e48a49
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-05-06 00:38:17 +0200

    rework netwars.path

--- src/clj/netwars/path.clj
+++ src/clj/netwars/path.clj
@@ -2,31 +2,11 @@
   (:require [netwars.game-board :as board]
             [netwars.aw-map :as aw-map]))
 
-(deftype AwPath [coordinates]
-  Object
-  (toString [this] (str "<AwPath [" (apply str (map (juxt :x :y) coordinates)) "]>"))
-  clojure.lang.IPersistentMap
-  clojure.lang.ILookup
-  (valAt [this key] (when (= key :coordinates) coordinates))
-  (valAt [this key notfound] (if (= key :coordinates) coordinates notfound))
-  clojure.lang.IPersistentCollection
-  (count [this] (.count coordinates))
-  (empty [this]  (AwPath. nil))
-  (cons [this e]  (AwPath. (.cons coordinates e)))
-  (equiv [this gs] (or (identical? this gs)
-                       (when (identical? (class this) (class gs))
-                         (= coordinates (.coordinates gs)))))
-  clojure.lang.Seqable
-  (seq [this] (seq coordinates)))
-
-(when clojure.core/print-method
-  (defmethod clojure.core/print-method AwPath [o p]
-   (.write p (.toString o))))
-
 (defn path?
   "Predicate to test if an object is a path."
   [p]
-  (instance? AwPath p))
+  (and (vector? p)
+       (every? aw-map/coord? p)))
 
 (defn valid-path?
   "Checks if a path is valid.
@@ -52,9 +32,4 @@ With the optional second argument, it checks for validity in the context of the 
 (defn make-path
   "Creates a path from a list of coordinates"
   [coords]
-  (AwPath. coords))
-
-(defn get-coordinates
-  "Returns the seq of coordinates of the path"
-  [path]
-  (:coordinates path))
+  (vec coords))
--- test/netwars/test/path.clj
+++ test/netwars/test/path.clj
@@ -2,29 +2,26 @@
   (:use clojure.test
         [netwars.aw-map :only [coord]]
         netwars.path
-        [netwars.test.game-board :only [make-testboard]])
-  (:import netwars.path.AwPath))
+        [netwars.test.game-board :only [make-testboard]]))
 
 (deftest test-make-path
-  (let [coords (map coord [[1 1] [1 2] [1 3]])]
-   (is (instance? netwars.path.AwPath (make-path coords)))
-   (is (= coords (:coordinates (make-path coords))))))
+  (is (path? (make-path (map coord [[1 1] [1 2] [1 3]])))))
 
 (deftest test-valid-path
-  (let [path (AwPath. (map coord [[1 1] [1 2] [1 3]]))]
-    (is (not (valid-path? (AwPath. []))))
-    (is (not (valid-path? (AwPath. (map coord [[1 1]])))))
+  (let [path (make-path (map coord [[1 1] [1 2] [1 3]]))]
+    (is (not (valid-path? (make-path []))))
+    (is (not (valid-path? (make-path (map coord [[1 1]])))))
     (testing "valid paths"
       (is (valid-path? path)))
     (testing "general validity"
       (testing "too short paths"
-        (is (not (valid-path? (AwPath. (map coord [[1 1] [1 2] [1 4]])))))
-        (is (not (valid-path? (AwPath. [1 2])))))
-      (is (not (valid-path? (AwPath. (map coord [[1 1] [1 2] [1 1]]))))
+        (is (not (valid-path? (map coord [[1 1] [1 2] [1 4]]))))
+        (is (not (valid-path? [1 2]))))
+      (is (not (valid-path? (map coord [[1 1] [1 2] [1 1]])))
           "Fields are unique in path"))
     (testing "with optional board-argument"
       (let [board (make-testboard)
-            valid (AwPath. (map coord [[1 11] [1 12] [1 13]]))]
+            valid (map coord [[1 11] [1 12] [1 13]])]
         (is (not (valid-path? (rest valid) board))
             "should return false when start coordinate is empty")
         (is (not (valid-path? valid board))
@@ -34,19 +31,15 @@
                               board))
             "should return false when the path is longer than remaining fuel or movement-range")
         (testing "if fields in path are traversable"
-         (is (not (valid-path? (AwPath. (map coord [[1 11] [1 12] [1 13]]))
+         (is (not (valid-path? (make-path (map coord [[1 11] [1 12] [1 13]]))
                                board))
              "BM Artillery can't traverse field with OS infantry on it")
-         (is (not (valid-path? (AwPath. (map coord [[1 11] [1 10]]))
+         (is (not (valid-path? (make-path (map coord [[1 11] [1 10]]))
                                board))
              "BM Artillery can't traverse mountain terrain")
-         (is (valid-path? (AwPath. (map coord [[1 11] [1 12] [2 12] [3 12]]))
+         (is (valid-path? (make-path (map coord [[1 11] [1 12] [2 12] [3 12]]))
                           board))
          "BM Artillery can traverse plain and street")))))
-
-(deftest test-get-coordinates
-  (let [coords (map coord [[1 1] [1 2] [1 3]])]
-   (is (= coords (get-coordinates (make-path coords))))))
 
 (deftest test-sequence-interface
   (let [coords (map coord [[1 1] [1 2] [1 3]])]
