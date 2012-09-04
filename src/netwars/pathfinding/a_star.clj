49c7fe076efbd545fca82046e5e0816a11c6dd18
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-09-04 15:12:49 +0200

    Reverting "Functional pathfinding" Revert "Functional pathfinding"
    This reverts commit b2998d5d8452c5f2808af620104fcd2813a06a86.

--- src/netwars/pathfinding/a_star.clj
+++ src/netwars/pathfinding/a_star.clj
@@ -6,8 +6,8 @@
 
 (defn- neighbours [movement-range c]
   (let [{:keys [x y]} c]
-    (filter movement-range (map (fn [[dx dy]] (aw-map/coord (+ x dx) (+ y dy)))
-                                [[0 1] [1 0] [-1 0] [0 -1]]))))
+   (filter movement-range (map (fn [[dx dy]] (aw-map/coord (+ x dx) (+ y dy)))
+                               [[0 1] [1 0] [-1 0] [0 -1]]))))
 
 ;;; TODO: Handle cyclic paths
 (defn- reconstruct-path [came-from start goal]
@@ -22,63 +22,58 @@
   (assert (contains? movement-range start))
   (assert (contains? movement-range end))
   (let [movement-type (:movement-type (meta (board/get-unit board start)))
-        h (fn [[c _]] (* 2 (aw-map/distance c end)))
-        f (fn [[c [costs _]]] (+ costs (* 2 (aw-map/distance c end))))
-        closed {}
-        open (sorted-set-by #(cond
-                              (= %1 %2) 0
-                              (not= %1 %2) (if (= (f %1) (f %2))
-                                             (if (< (h %1) (h %2))
-                                               -1 1)
-                                             (if (< (f %1) (f %2))
-                                               -1 1))
-                              true 1)
-                            [start [0 nil]])]
-    (reconstruct-path
-     (loop [closed closed
-            open open
-            limit 50]
-       (if-let [[c [pcosts p]] (first open)]
-         (if-not (= c end)
-           (let [closed (assoc closed c p)
-                 edges (remove (partial contains? closed) (neighbours movement-range c))
-                 open (reduce (fn [open edge]
-                                (let [costs (+ pcosts
-                                               (aw-map/movement-costs
-                                                (board/get-terrain board edge)
-                                                movement-type))]
-                                  (if-let [oedge (some #(when (= edge (first %)) %) open)]
-                                    (let [[c [oldcosts p]] oedge]
-                                      (if (< costs oldcosts)
-                                        (conj (disj open oedge) [c [costs p]])
-                                        open))
-                                    (conj open [edge [costs c]]))))
-                              (disj open (first open))
-                              edges)]
-             (recur closed open (dec limit)))
-           (assoc closed c p))))
-     start end)))
+        g (atom {start 0})
+        h #(* 2 (aw-map/distance % end))
+        f #(+ (get @g % 0) (h %))
+        closedset #{}
+        openset (sorted-set-by #(compare (f %1) (f %2)) start)
+        came-from {}]
+    (loop [g g
+           closedset closedset
+           openset openset
+           came-from came-from]
+      (let [x (first openset)
+            closedset (conj closedset x)
+            edges (remove closedset (neighbours movement-range x))
+
+            [g came-from openset]
+            (reduce (fn [[g came-from openset] edge]
+                      (let [path-costs (+ (aw-map/movement-costs
+                                           (board/get-terrain board edge)
+                                           movement-type)
+                                          (get @g x 0))]
+                        (if-not (contains? openset edge)
+                          [(do (swap! g assoc edge path-costs) g)
+                           (assoc came-from edge x)
+                           (conj openset edge)]
+                          [(if (< path-costs (get @g x))
+                             (do (swap! g assoc path-costs) g)
+                             g)
+                           came-from
+                           openset])))
+                    [g came-from (disj openset x)] edges)]
+        (if (or (contains? closedset end) (empty? openset))
+          (reconstruct-path came-from start end)
+          (recur g closedset openset came-from))))))
 
 
-;; (comment
-;;   (let [board (:board (netwars.game-creator/make-game {} "7330.aws"))
-;;         start (aw-map/coord 3 14)
-;;         end (aw-map/coord 0 12)
-;;         r (board/reachable-fields board start)]
-;;     (defn test-path []
-;;       (a-star-path board r start end))
-;;     (defn test-a-star []
-;;       (= [#netwars.aw_map.Coordinate{:x 3, :y 14}
-;;           #netwars.aw_map.Coordinate{:x 2, :y 14}
-;;           #netwars.aw_map.Coordinate{:x 2, :y 13}
-;;           #netwars.aw_map.Coordinate{:x 1, :y 13}
-;;           #netwars.aw_map.Coordinate{:x 0, :y 13}
-;;           #netwars.aw_map.Coordinate{:x 0, :y 12}]
-;;           (test-path)))
+(comment
+  (let [board (:board (netwars.game-creator/make-game {} "7330.aws"))
+        start (aw-map/coord 3 14)
+        end (aw-map/coord 0 12)
+        r (board/reachable-fields board start)]
+    (defn test-a-star []
+      (= [#netwars.aw_map.Coordinate{:x 3, :y 14}
+          #netwars.aw_map.Coordinate{:x 2, :y 14}
+          #netwars.aw_map.Coordinate{:x 2, :y 13}
+          #netwars.aw_map.Coordinate{:x 1, :y 13}
+          #netwars.aw_map.Coordinate{:x 0, :y 13}
+          #netwars.aw_map.Coordinate{:x 0, :y 12}]
+          (a-star-path board r start end)))
 
-;;     (defn test-performance-path []
-;;       (print "Path: ")
-;;       (doseq [c (a-star-path board r start end)] (print " ->" (str "(" (:x c) "," (:y c) ")")))
-;;       (prn)
-;;       (time
-;;        (dotimes [_ 1000] (a-star-path board r start end))))))
+    (defn test-performance-path []
+      (print "Path: ")
+      (doseq [c (a-star-path board r start end)] (print " ->" (str "(" (:x c) "," (:y c) ")")))
+      (prn)
+      (time
+       (dotimes [_ 1000] (a-star-path board r start end))))))
