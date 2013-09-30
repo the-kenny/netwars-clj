bc5b604dc81ae7cc555a3e966f7eb5c4ed3b54e8
Moritz Ulrich <moritz@tarn-vedra.de>
2013-09-30 20:21:41 +0200

    Update dependencies.
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- project.clj
+++ project.clj
@@ -2,11 +2,12 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [commons-codec "1.5" :exclusions [commons-logging]]
                  [compojure "1.1.5"]
-                 [hiccup "1.0.3"]
-                 [http-kit "2.1.6"]
+                 [hiccup "1.0.4"]
+                 [http-kit "2.1.10"]
                  [ring/ring-core "1.2.0"]
-                 [ring-edn "0.1.0"]
-                 [org.clojure/tools.nrepl "0.2.3"]]
+                 [fogus/ring-edn "0.2.0"]
+                 [org.clojure/tools.nrepl "0.2.3"]
+                 [org.clojure/clojurescript "0.0-1909"]]
   :extra-files-to-clean ["resoures/public/netwars.js"]
   :main netwars.core
   :repl-options {:port 7888}
@@ -18,18 +19,17 @@
       :output-to "resources/public/netwars.js",
       :warnings true,
       :optimizations :whitespace}}],
-   :crossovers
-   [netwars.aw-game
-    netwars.aw-map
-    netwars.aw-player
-    netwars.aw-unit
-    netwars.damagecalculator
-    netwars.game-board
-    netwars.map-utils
-    netwars.map-renderer
-    netwars.path
-    netwars.pathfinding.a-star
-    netwars.tiles]}
+   :crossovers [netwars.aw-game
+                netwars.aw-map
+                netwars.aw-player
+                netwars.aw-unit
+                netwars.damagecalculator
+                netwars.game-board
+                netwars.map-utils
+                netwars.map-renderer
+                netwars.path
+                netwars.pathfinding.a-star
+                netwars.tiles]}
 
-  :profiles {:dev {:dependencies [[clojure-complete "0.2.2"]]}}
-  :plugins [[lein-cljsbuild "0.3.2"]])
+  :profiles {:dev {:dependencies [[clojure-complete "0.2.3"]]}}
+  :plugins [[lein-cljsbuild "0.3.3"]])
